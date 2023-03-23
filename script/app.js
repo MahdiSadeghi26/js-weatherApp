28a9be0a0bff1fb9c32855a9e61147e849b70ac0
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-03-23 20:46:08 +0330

    adding fetch and applying value to loca variabels

--- script/app.js
+++ script/app.js
@@ -15,9 +15,25 @@ const date=$.querySelector('.date')
 const temp=$.querySelector('.degree')
 const weatherType=$.querySelector('.weather-type')
 // funcitons
-
+function adingWeatherToDom(api) {
+    if (api.cod===200) {
+        console.log(api)
+        // changing name and region to api data
+        regionHeading.innerHTML=`${api.name} , ${api.sys.country}`
+        // applying current date
+        date.innerHTML=new Date().toDateString()
+        // applying temp and truncing
+        temp.innerHTML=`${Math.trunc(api.main.temp)}°C`;
+        // applying weather type
+        weatherType.innerHTML=api.weather[0].main
+        
+        
+    }
+}
 // event listeners
 window.addEventListener('load',()=>{
     inputValue.value=''
-    
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=ab75a49fee712fae47e85b0033b176cd&units=metric")
+    .then(res=>res.json())
+    .then(data=>adingWeatherToDom(data))
 })
